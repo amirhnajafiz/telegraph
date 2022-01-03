da9c0c547810e33cfc7ebaaa6dcd612619d60e5a
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-03 14:47:52 +0330

    koanf loag yml added

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,43 @@
 package config
 
-import "Telegraph/internal/logger"
+import (
+	"Telegraph/internal/logger"
+	"encoding/json"
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+	"log"
+)
 
 type (
 	Config struct {
 		Logger logger.Config `koanf:"logger"`
 	}
 )
+
+func Load() Config {
+	var instance Config
+
+	k := koanf.New(".")
+
+	// load default configuration from default function
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		log.Fatalf("error loading default: %s", err)
+	}
+
+	// load configuration from file
+	if err := k.Load(file.Provider("config.yml"), yaml.Parser()); err != nil {
+		log.Printf("error loading config.yml: %s", err)
+	}
+
+	indent, _ := json.MarshalIndent(instance, "", "\t")
+	tmpl := `
+	================ Loaded Configuration ================
+	%s
+	======================================================
+	`
+	log.Printf(tmpl, string(indent))
+
+	return instance
+}
