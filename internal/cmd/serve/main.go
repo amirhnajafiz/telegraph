c5d905c0454db6eb409ad41ee269c22314d9aceb
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-04 14:02:37 +0330

    handlers init

--- internal/cmd/serve/main.go
+++ internal/cmd/serve/main.go
@@ -3,15 +3,12 @@ package serve
 import (
 	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
-	"net/http"
 )
 
 func GetServer(logger *zap.Logger) *echo.Echo {
 	logger.Info("Initialized server")
 
 	e := echo.New()
-	e.GET("api/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+
 	return e
 }
---
+++ internal/handler/api/root.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+type Root struct {
+	Logger *zap.Logger
+}
+
+func (root Root) Handle(c echo.Context) error {
+	return c.String(http.StatusOK, "Welcome home")
+}
+
+func (root Root) Register(g echo.Group) {
+	g.GET("/", root.Handle)
+}
