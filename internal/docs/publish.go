0bbbb678cc32860fd55504639b71d457a4fa7195
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-05 14:07:08 +0330

    docs init with publish

---
+++ internal/docs/publish.go
@@ -0,0 +1,14 @@
+package docs
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Publish struct {
+	ID   primitive.ObjectID `bson:"_id,omitempty"`
+	From string             `bson:"from,omitempty"`
+	To   string             `bson:"to,omitempty"`
+	Msg  string             `bson:"msg,omitempty"`
+	Date time.Time          `bson:"date,omitempty"`
+}
