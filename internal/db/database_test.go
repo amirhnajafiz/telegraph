624d0e28408cb462a05fe57ed3fb805072b49ab5
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-07 10:25:59 +0330

    mongo testing completed

--- internal/db/database_test.go
+++ internal/db/database_test.go
@@ -11,7 +11,7 @@ import (
 
 func TestNewDB(t *testing.T) {
 	opts := options.Client()
-	opts.ApplyURI(cfg.URL)
+	opts.ApplyURI("mongodb://127.0.0.1:27017")
 
 	client, err := mongo.NewClient(opts)
 	if err != nil {
@@ -34,6 +34,16 @@ func TestNewDB(t *testing.T) {
 
 		if err := client.Ping(ctx, readpref.Primary()); err != nil {
 			assert.Error(t, err, "database ping error")
+		}
+	}
+	// Closing connection
+	{
+		ctx, done := context.WithTimeout(context.Background(), connectionTimeout)
+		defer done()
+
+		err := client.Disconnect(ctx)
+		if err != nil {
+			assert.Fail(t, "disconnection client failed")
 		}
 	}
 }
