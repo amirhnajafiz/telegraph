755ac8fd4bfcd67b2e09e803ece030fe6db2331e
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-07 10:21:47 +0330

    database testing for mongo added

--- internal/db/database_test.go
+++ internal/db/database_test.go
@@ -1,10 +1,39 @@
 package db
 
 import (
+	"context"
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"testing"
 )
 
 func TestNewDB(t *testing.T) {
-	assert.True(t, true, "This is true")
+	opts := options.Client()
+	opts.ApplyURI(cfg.URL)
+
+	client, err := mongo.NewClient(opts)
+	if err != nil {
+		assert.Fail(t, "Creating db client failed", err)
+	}
+
+	// connect to the mongodb
+	{
+		ctx, done := context.WithTimeout(context.Background(), connectionTimeout)
+		defer done()
+
+		if err := client.Connect(ctx); err != nil {
+			assert.Error(t, err, "database connection error")
+		}
+	}
+	// ping the mongodb
+	{
+		ctx, done := context.WithTimeout(context.Background(), connectionTimeout)
+		defer done()
+
+		if err := client.Ping(ctx, readpref.Primary()); err != nil {
+			assert.Error(t, err, "database ping error")
+		}
+	}
 }
