3c337700d46fb2554908d0673fdd86f709fd76c2
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-08 19:46:44 +0330

    collection storing method added

--- internal/store/publish/publish.go
+++ internal/store/publish/publish.go
@@ -1,11 +1,29 @@
 package publish
 
 import (
+	"Telegraph/internal/docs"
 	"Telegraph/internal/handler/api"
 	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
 )
 
-type Publish interface {
-	Store(ctx context.Context, key string) error
-	All(ctx context.Context, key string) ([]api.Publish, error)
+var collection = "publications"
+
+func Store(database *mongo.Database, ctx context.Context, r api.Request) error {
+	col := database.Collection(collection)
+
+	item := &docs.Publish{
+		From: r.Source,
+		To:   r.Des,
+		Msg:  r.Msg,
+		Date: time.Now(),
+	}
+
+	_, err := col.InsertOne(ctx, item)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
