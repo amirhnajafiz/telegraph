cbdc28b04f4d2c89aed3d40d3e224da21f2f83c8
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-08 20:22:30 +0330

    suppress all get method added

--- internal/handler/suppress/suppress.go
+++ internal/handler/suppress/suppress.go
@@ -1,10 +1,13 @@
 package suppress
 
 import (
+	"Telegraph/internal/store/message"
 	"context"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
+	"net/http"
 	"time"
 )
 
@@ -14,7 +17,13 @@ type Suppress struct {
 }
 
 func (s Suppress) Handle(c echo.Context) error {
+	col := s.Database.Collection(message.Collection)
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
-	return nil
+	cursor, _ := col.Find(ctx, bson.D{})
+	var results []message.Message
+
+	cursor.All(ctx, results)
+
+	return c.JSON(http.StatusOK, results)
 }
