f1a1b53e0236c3b9e064341fee72394eb96c0fe1
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-12 20:49:51 +0330

    request suppress method added

---
+++ internal/http/request/suppress.go
@@ -0,0 +1,22 @@
+package request
+
+import (
+	"Telegraph/pkg/validate"
+	"github.com/labstack/echo/v4"
+	"github.com/thedevsaddam/govalidator"
+	"net/url"
+)
+
+func SuppressValidate(r echo.Context) (url.Values, map[string]interface{}) {
+	rules := govalidator.MapData{
+		"sender": []string{"required", "between:4,20"},
+	}
+
+	opts := govalidator.Options{
+		Request:         r.Request(), // request object
+		Rules:           rules,       // rules map
+		RequiredDefault: true,        // all the field to be pass the rules
+	}
+
+	return validate.Do(opts, validate.InputType)
+}
--- pkg/validate/publish.go
+++ pkg/validate/publish.go
@@ -5,12 +5,26 @@ import (
 	"net/url"
 )
 
-func Do(opts govalidator.Options) (url.Values, map[string]interface{}) {
+var (
+	JsonType   = "json"
+	InputType  = "input"
+	StructType = "struct"
+)
+
+func Do(opts govalidator.Options, validateType string) (url.Values, map[string]interface{}) {
 	data := make(map[string]interface{})
 	opts.Data = data
 
 	v := govalidator.New(opts)
-	e := v.ValidateJSON()
+
+	var e url.Values
+	if validateType == JsonType {
+		e = v.ValidateJSON()
+	} else if validateType == InputType {
+		e = v.Validate()
+	} else if validateType == StructType {
+		e = v.ValidateStruct()
+	}
 
 	return e, data
 }
