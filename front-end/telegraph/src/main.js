c87175e347db3249a27a495961ae7ffaede0ab9d
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-02 09:24:45 +0330

    front-end expose

--- front-end/telegraph/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import axios from "axios";
-
-axios.defaults.headers.common['Content-Type'] ='application/x-www-form-urlencoded';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-
-createApp(App).mount('#app')
