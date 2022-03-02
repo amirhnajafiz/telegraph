2a762438148e11e535b746f4837600621606eebd
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-02 14:39:19 +0330

    migrate and serve main remove

--- internal/cmd/migrate/migrate.go
+++ internal/cmd/migrate/migrate.go
@@ -1,6 +1,7 @@
 package migrate
 
 import (
+	store2 "github.com/amirhnajafiz/Telegraph/internal/db/store"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 )
@@ -8,4 +9,11 @@ import (
 type Migrate struct {
 	Database *mongo.Database
 	Logger   *zap.Logger
+}
+
+func (m Migrate) Do() {
+	_ = m.Database.Collection(store2.MessageCollection)
+	_ = m.Database.Collection(store2.UserCollection)
+
+	m.Logger.Info("collections created")
 }
--- internal/cmd/serve/serve.go
+++ internal/cmd/serve/serve.go
@@ -1,6 +1,9 @@
 package serve
 
 import (
+	"github.com/amirhnajafiz/Telegraph/internal/http/handler"
+	"github.com/amirhnajafiz/Telegraph/pkg/validate"
+	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 )
@@ -8,4 +11,17 @@ import (
 type Serve struct {
 	Database *mongo.Database
 	Logger   *zap.Logger
+}
+
+func (s Serve) GetServer() *echo.Echo {
+	s.Logger.Info("Initialized server")
+
+	e := echo.New()
+	handler.Handler{
+		Database: s.Database,
+		Logger:   s.Logger.Named("handler"),
+		Validate: validate.Validate{},
+	}.Set(e)
+
+	return e
 }
