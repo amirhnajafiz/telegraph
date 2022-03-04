8e26a43dccbfda69e06e46d54df755abd4ae9ef5
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-04 19:06:33 +0330

    jwt generate token

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,5 +1,30 @@
 package jwt
 
-func GenerateToken(user string) {
+import (
+	"github.com/golang-jwt/jwt"
+	"time"
+)
+
+var jwtKey = []byte("my_secret_key")
+
+type JWT struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(user string) (string, error) {
 	// create token
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &JWT{
+		Username: user,
+		StandardClaims: jwt.StandardClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Create the JWT string
+	return token.SignedString(jwtKey)
 }
