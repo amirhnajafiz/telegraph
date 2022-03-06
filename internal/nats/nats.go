9fad4e78f0f6247880ae35243fa23ce6d477be12
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-06 18:39:13 +0330

    nats new method for nats connection added

--- internal/nats/nats.go
+++ internal/nats/nats.go
@@ -1,1 +1,20 @@
 package nats
+
+import (
+	"github.com/nats-io/nats.go"
+	"go.uber.org/zap"
+)
+
+type Nats struct {
+	Logger *zap.Logger
+	Cfg    Config
+}
+
+func (n Nats) New() *nats.Conn {
+	nc, err := nats.Connect(n.Cfg.Host)
+	if err != nil {
+		n.Logger.Fatal("nats connection failed", zap.Error(err))
+	}
+
+	return nc
+}
