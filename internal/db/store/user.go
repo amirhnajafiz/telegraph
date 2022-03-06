a5c53aceb8afb8499c1dd6f64df8f8defdddb7d8
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-06 15:52:40 +0330

    username is set

--- internal/db/store/user.go
+++ internal/db/store/user.go
@@ -1,10 +1,26 @@
 package store
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 var UserCollection = "clients"
 
 type Client struct {
 	ID   primitive.ObjectID `bson:"_id,omitempty"`
 	Name string             `bson:"from,omitempty"`
+}
+
+func (c Client) Store(database *mongo.Database, ctx context.Context, client *Client) error {
+	col := database.Collection(UserCollection)
+
+	_, err := col.InsertOne(ctx, client)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
