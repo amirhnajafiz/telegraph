18bbbf54bd68ee99fff07a726852bba4046664da
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-06 16:29:59 +0330

    join logic is implemented

--- internal/http/handler/join.go
+++ internal/http/handler/join.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"context"
+	"github.com/amirhnajafiz/Telegraph/internal/db/store"
 	"github.com/amirhnajafiz/Telegraph/pkg/jwt"
 	"github.com/amirhnajafiz/Telegraph/pkg/validate"
 	"github.com/labstack/echo/v4"
@@ -16,10 +18,35 @@ type Join struct {
 }
 
 func (j Join) Handle(c echo.Context) error {
-	user := c.QueryParam("username")
+	valid, data := j.Validate.PublishValidate(c)
+
+	if valid.Encode() != "" {
+		return c.JSON(http.StatusBadRequest, valid)
+	}
+
+	user := data["username"].(string)
+	pass := data["pass"].(string)
 	if user == "" {
 		c.Response().Status = http.StatusBadRequest
 		return c.String(http.StatusBadRequest, "no username found")
+	}
+
+	ctx := context.Background()
+	client, err := store.Client{}.Find(j.Database, ctx, user)
+	if err != nil {
+		return err
+	}
+
+	if client.Pass != "" && client.Pass != pass {
+		c.Response().Status = http.StatusUnauthorized
+		return c.String(http.StatusUnauthorized, "user and pass don't match")
+	}
+
+	if client.Pass == "" {
+		_ = store.Client{}.Store(j.Database, ctx, &store.Client{
+			Name: user,
+			Pass: pass,
+		})
 	}
 
 	token, err := jwt.GenerateToken(user)
@@ -34,5 +61,5 @@ func (j Join) Handle(c echo.Context) error {
 }
 
 func (j Join) Register(g *echo.Group) {
-	g.GET("/join", j.Handle)
+	g.POST("/join", j.Handle)
 }
