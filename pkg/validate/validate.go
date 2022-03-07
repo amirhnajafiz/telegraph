b83065e4a66b6559a1e2cc7c46e77d859cc80a7c
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-07 18:52:30 +0330

    publish validate refactor

--- pkg/validate/validate.go
+++ pkg/validate/validate.go
@@ -50,8 +50,7 @@ func (v Validate) SuppressValidate(r echo.Context) (url.Values, map[string]inter
 
 func (v Validate) PublishValidate(r echo.Context) (url.Values, map[string]interface{}) {
 	rules := govalidator.MapData{
-		"from":    []string{"required", "between:4,20"},
-		"to":      []string{"required", "between:4,20"},
+		"sender":  []string{"required", "between:4,20"},
 		"message": []string{"between:0,250"},
 	}
 
